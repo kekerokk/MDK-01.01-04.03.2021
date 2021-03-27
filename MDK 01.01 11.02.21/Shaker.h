0a907442f19fbdf22b5d44912f407b20049d5ef2
Иван <chelove4edd@mail.ru>
2021-03-27 16:38:41 +0300

    Добавьте файлы проекта.

---
+++ MDK 01.01 11.02.21/Shaker.h
@@ -0,0 +1,110 @@
+#pragma once
+#include <iostream>
+#include <list>
+#include <locale.h>
+using namespace std;
+template<typename T>
+class Shaker
+{
+public:
+	void add_element_front(list<T>& list, T element);
+	void print(list<T> list);
+	void del_element_front(list<T>& list);
+	int equality_test(list<T> list_first, list<T> list_second);
+	void add_element_in_pos(list<T>& list, T element, int pos);
+private:
+	typename list<T>::iterator Iter;
+	typename list<T>::iterator Iter2;
+	typename list<T>::iterator Class_Iter; // ипользовать для контейнера из класса
+	list<T> Class_MyList;
+};
+
+template<typename T>
+inline void Shaker<T>::add_element_in_pos(list<T>& list, T element, int pos)
+{
+	setlocale(LC_ALL, "ru");
+	for (Iter = list.begin(); Iter != list.end(); Iter++)
+	{
+		Class_MyList.push_back(*Iter);
+	}
+	Iter = list.begin();
+	Class_Iter = Class_MyList.begin();
+	int list_size = list.size();
+	list.clear();
+	for (int i = 0; i < list_size+1; i++, Class_Iter++)
+	{
+		if (i + 1 == pos)
+		{
+			list.push_back(element);
+			Class_Iter--;
+		}
+		else
+		{
+			list.push_back(*Class_Iter);
+		}
+	}
+	cout << "\nЭлемент добавлен во " << pos << " позицию.\n";
+}
+
+template<typename T>
+inline void Shaker<T>::add_element_front(list<T>& list, T element)
+{
+	list.push_front(element);
+}
+
+template<typename T>
+inline void Shaker<T>::print(list<T> list)
+{
+	setlocale(LC_ALL, "ru");
+	if (list.size() == 0)
+	{
+		cout << "Список пуст" << endl;
+	}
+	for (Iter = list.begin(); Iter != list.end(); Iter++)
+	{
+		cout << *Iter << endl;
+	}
+}
+
+template<typename T>
+inline void Shaker<T>::del_element_front(list<T>& list)
+{
+	list.pop_front();
+}
+
+template<typename T>
+inline int Shaker<T>::equality_test(list<T> list_first, list<T> list_second)
+{
+	setlocale(LC_ALL, "ru");
+	Iter = list_first.begin(), Iter2 = list_second.begin();
+	if (list_first.size() != 0 && list_second.size() != 0)
+	{
+		if (list_first.size() == list_second.size())
+		{
+			cout << "Проверка на равенство списков началась...\n" << endl;
+			for (Iter; Iter != list_first.end(); Iter++,Iter2++)
+			{
+				if (*Iter == *Iter2)
+				{
+				}
+				else
+				{
+					cout << "Списки не равны\n";
+					return -1;
+				}
+			}
+			cout << "Списки равны\n";
+			return 1;
+		}
+		else
+		{
+			cout << "Списки несоразмерны\n";
+			return -1;
+		}
+	}
+	else
+	{
+		cout << "Один или оба списков не заполнены\n";
+		return -1;
+	}
+}
---
+++ MDK 01.01 11.02.21/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+#include "Shaker.h"
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	list<int> MyList;
+	list<int> MyList2;
+	Shaker<int> b;
+	int x = 5;
+	int x2 = 3;
+	cout << "Добавление элемента...\n";
+	b.add_element_front(MyList, x);
+	cout << "Вывод...\n";
+	b.print(MyList);
+	cout << "Удаление элемента из начала...\n";
+	b.del_element_front(MyList);
+	cout << "Вывод...\n";
+	b.print(MyList);
+	cout << "Добавление элементов в списки...\n";
+	b.add_element_front(MyList2, x);
+	b.add_element_front(MyList, x);
+	b.equality_test(MyList, MyList2);
+	b.add_element_front(MyList, x2);
+	cout << "Добавление элемента в список в определенную позицию\n";
+	b.add_element_in_pos(MyList, 4, 2);
+	cout << "Вывод...\n";
+	b.print(MyList);
+}
